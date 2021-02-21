9926ab865f0c9204a278c99d27aa93f6c92c272f
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-21 17:28:56 +0530

    Added code to compute Wages for Part-Time Employee

--- emp_wage_computation.java
+++ emp_wage_computation.java
@@ -2,21 +2,28 @@ public class emp_wage_computation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation problem");
 
-		int IS_PRESENT=1;
+		int is_FullTime=1;
+		int is_PartTime=2;
 		double wagePerHour=20;
-		double fullDayHour=8;
 		double empWage;
-		double empCheck=Math.floor(Math.random() * 10 ) %2;
+		double empCheck=Math.floor(Math.random() * 10 ) %3;
 
-
-		if (empCheck==IS_PRESENT){
-			System.out.println("Employee is present");
-			empWage=wagePerHour*fullDayHour;
+		if (empCheck==is_FullTime){
+			System.out.println("Employee is Full Time");
+			double dailyHour=8;
+			empWage=wagePerHour*dailyHour;
 			System.out.println("Employee wage is: "+ empWage);
 		}
-		else{
-			System.out.println("Employee is absent");
-			empWage=wagePerHour*0;
+		else if (empCheck==is_PartTime){
+			System.out.println("Employee is Part Time");
+			double dailyHour=4;
+			empWage=wagePerHour*dailyHour;
+			System.out.println("Employee wage is: "+ empWage);
+		}
+		else {
+			System.out.println("Employee is Absent");
+			double dailyHour=0;
+			empWage=wagePerHour*dailyHour;
 			System.out.println("Employee wage is: "+ empWage);
 		}
 	}
