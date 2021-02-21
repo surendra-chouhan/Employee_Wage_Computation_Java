319d1db6b547f21453b540736fe1b46856bcd33d
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-21 17:10:20 +0530

    Added code to calculate Daily Wage

--- emp_wage_computation.java
+++ emp_wage_computation.java
@@ -2,14 +2,22 @@ public class emp_wage_computation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation problem");
 
-		int isfulltime=1;
+		int IS_PRESENT=1;
+		double wagePerHour=20;
+		double fullDayHour=8;
+		double empWage;
+		double empCheck=Math.floor(Math.random() * 10 ) %2;
 
-   	int empcheck = (int)Math.floor(Math.random() * 10)%2;
 
-   	if (empcheck == isfulltime)
-      	System.out.println("Employee is present");
-   	else
-     		System.out.println("Employee is absent");
-
+		if (empCheck==IS_PRESENT){
+			System.out.println("Employee is present");
+			empWage=wagePerHour*fullDayHour;
+			System.out.println("Employee wage is: "+ empWage);
+		}
+		else{
+			System.out.println("Employee is absent");
+			empWage=wagePerHour*0;
+			System.out.println("Employee wage is: "+ empWage);
+		}
 	}
 }
