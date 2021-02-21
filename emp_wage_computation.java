a26d9c95befc85b7f5a08ed3a405e0918962fa11
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-21 17:53:15 +0530

    Added code to compute Monthly Wages

--- emp_wage_computation.java
+++ emp_wage_computation.java
@@ -5,30 +5,30 @@ public class emp_wage_computation {
 		int is_FullTime=1;
 		int is_PartTime=2;
 		double wagePerHour=20;
-		double empWage;
+		double dailyWage;
 		double dailyHour;
+		int max_days_in_a_month=20;
 		int empCheck=(int)Math.floor(Math.random() * 10 ) %3;
 
 		switch (empCheck) {
 			case 1:
 				System.out.println("Employee is Full Time");
 				dailyHour=8;
-				empWage=wagePerHour*dailyHour;
-				System.out.println("Employee wage is: "+ empWage);
+				dailyWage=wagePerHour*dailyHour;
+				System.out.println("Employee wage is: "+ dailyWage);
+				System.out.println("Monthly Wage is : "+ dailyWage*max_days_in_a_month);
 				break;
 
 			case 2:
 				System.out.println("Employee is Part Time");
 				dailyHour=4;
-				empWage=wagePerHour*dailyHour;
-				System.out.println("Employee wage is: "+ empWage);
+				dailyWage=wagePerHour*dailyHour;
+				System.out.println("Employee wage is: "+ dailyWage);
+				System.out.println("Monthly Wage is : "+ dailyWage*max_days_in_a_month);
 				break;
 
 			case 0:
 				System.out.println("Employee is Absent");
-				dailyHour=0;
-				empWage=wagePerHour*dailyHour;
-				System.out.println("Employee wage is: "+ empWage);
 		}
 	}
 }
